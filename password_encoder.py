591ac2b32d56e839f2833618f489d16f020d7cf9
Vladislav <vladsmirn289@gmail.com>
2021-12-16 20:22:44 +0300

    Second task (multiplying each by next incremented by 1)

--- password_encoder.py
+++ password_encoder.py
@@ -5,3 +5,11 @@ coded_letters = list()
 # Part 1
 for i in key_word:
     coded_letters.append(ord(i))
+
+# Part 2
+coded_letters.append(coded_letters[0])
+list_size = len(coded_letters)
+for i in range(0, list_size - 1):
+    coded_letters[i] = coded_letters[i] * (coded_letters[i+1] + 1)
+
+coded_letters = coded_letters[0:list_size-1]
