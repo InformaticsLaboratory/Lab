c51327f6bbc1d794191e1005cb149dffc644a565
Vladislav <vladsmirn289@gmail.com>
2021-12-18 13:42:06 +0300

    Third task (sum all multiplied by ten in minus position)

--- password_encoder.py
+++ password_encoder.py
@@ -1,3 +1,5 @@
+from decimal import *
+
 print("Enter the keyword: ", end='')
 key_word = input()
 coded_letters = list()
@@ -7,9 +9,29 @@ for i in key_word:
     coded_letters.append(ord(i))
 
 # Part 2
-coded_letters.append(coded_letters[0])
 list_size = len(coded_letters)
-for i in range(0, list_size - 1):
+coded_letters.append(coded_letters[0])
+for i in range(0, list_size):
     coded_letters[i] = coded_letters[i] * (coded_letters[i+1] + 1)
 
-coded_letters = coded_letters[0:list_size-1]
+coded_letters = coded_letters[0:list_size]
+
+# Part 3
+encoded_number = 0
+for i in range(0, list_size):
+    coded_letters[i] = str(coded_letters[i]) + "0" * (list_size - i - 1)
+    encoded_number += int(coded_letters[i])
+
+encoded_number = list(str(encoded_number))
+encoded_number_size = len(encoded_number)
+if not list_size <= 0:
+    if encoded_number_size > list_size:
+        encoded_number.insert(encoded_number_size - list_size, ",")
+    elif encoded_number_size < list_size:
+        for i in range(0, list_size - encoded_number_size):
+            encoded_number.insert(0, "0")
+        encoded_number.insert(0, ",")
+        encoded_number.insert(0, "0")
+    else:
+        encoded_number.insert(0, ",")
+        encoded_number.insert(0, "0")
