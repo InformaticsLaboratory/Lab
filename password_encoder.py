8b057f2e915427edbb1681517b514cf647fe31a9
Cu11inan <sash4lol@yandex.ru>
2021-12-19 17:36:47 +0300

    The fourth task (convert numbers on edges to the average of others)

--- password_encoder.py
+++ password_encoder.py
@@ -1,22 +1,24 @@
 def convert_num(string):
-    sum = 0
+    am = 0
     avr = 0
     spl = []
     new_x = ''
     new_y = ''
 
+    if not(',' in string):
+        string += ',0'
+
     spl = string.split(',')
-    print(spl)
     avr_str = spl[1][0:-1]
     len_str = len(avr_str)
 
     if len_str > 0:
         for i in range(len_str):
             if avr_str[i] != ',':
-                sum += int(avr_str[i])
+                am += int(avr_str[i])
             else:
                 len_str -=1
-        avr = int(sum//len_str)
+        avr = int(am//len_str)
         if avr < 2:
             avr = 2
     else:
@@ -24,20 +26,23 @@ def convert_num(string):
 
     x = int(spl[0])
     y = int(spl[1][-1:-2:-1])
-    print('1')
-    while y != 0:
-        new_y += str(y%avr)
-        y //= avr
-    print('2')
+
     while x != 0:
         new_x += str(x%avr)
         x //= avr
 
+    while y != 0:
+        new_y += str(y%avr)
+        y //= avr
+
+    if len(new_x) == 0:
+        new_x = '0'
+    if len(new_y) == 0:
+        new_y = '0'
 
     new_x = new_x[::-1]
     new_y = new_y[::-1]
     new_string = new_x + ',' + spl[1]
     new_string = new_string[0:-1] + '+' + new_y
-    print(new_string)
+
     return new_string
-print(convert_num("2489357,34289529345"))
