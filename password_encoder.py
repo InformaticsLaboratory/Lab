139000d7dff73df636f11655e080bc7426320ebe
Max <makozer@yandex.ru>
2021-12-19 19:53:05 +0300

    fix covert_to_binary

--- password_encoder.py
+++ password_encoder.py
@@ -1,15 +1,20 @@
 def convert_to_binary(number):
     number = int(number)
+    print(number)
     binary = ""
     while number // 2 != 0:
+        print(number)
         binary += str(number % 2)
         number //= 2
     binary += str(number % 2)
+
+    binary = binary[::-1]
+
     if binary[0] == '0':
         binary = binary[1:]
     if binary == '':
         binary = "0"
-    return binary[::-1]
+    return binary
 
 
 def move_right_part(code_word):
@@ -19,10 +24,9 @@ def move_right_part(code_word):
         right_part += code_word[-1]
         code_word = code_word[:-1]
     right_part = right_part[::-1]
+
     right_part = convert_to_binary(right_part)
 
     left_part = convert_to_binary(code_word[:code_word.find(',')]) + (code_word[code_word.find(','):])
 
     return right_part + '-' + left_part
-
-
